e6d33224ec229a575f002501522dea819ef80dfb
Jacques <jak.rag@gmail.com>
2018-09-06 15:12:15 +0200

    Testing makeDeposit with value twice

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,26 +1,38 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
+    TransactionsFake transactionsFake;
+    Account account;
+
+    @Before
+    public void varInit(){
+        transactionsFake = new TransactionsFake();
+        account = new Account(transactionsFake);
+    }
     @Test
     public void make_deposit_should_delegate_to_transaction(){
-        TransactionsFake transactionsFake = new TransactionsFake();
-        Account account = new Account(transactionsFake);
-
         account.makeDeposit(new Amount(0));
 
         assertThat(transactionsFake.balance).isEqualTo(0);
     }
     @Test
     public void make_deposit_has_to_run_with_value(){
-        TransactionsFake transactionsFake = new TransactionsFake();
-        Account account = new Account(transactionsFake);
 
         account.makeDeposit(new Amount(100));
 
         assertThat(transactionsFake.balance).isEqualTo(100);
+    }
+    @Test
+    public void make_deposite_can_agregate_amount(){
+
+        account.makeDeposit(new Amount(100));
+        account.makeDeposit(new Amount(100));
+
+        assertThat(transactionsFake.balance).isEqualTo(200);
     }
 
 
@@ -28,10 +40,10 @@ public class AccountTest {
 
 
     class TransactionsFake implements Transactions{
-        public int balance;
+        public int balance = 0;
 
         public void add(Amount amount) {
-            balance = amount.value;
+            balance += amount.value;
         }
     }
 }
