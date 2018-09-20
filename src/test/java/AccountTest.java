ae11a46413f494d2a424486fb84eb66d3171bddf
Jacques <jak.rag@gmail.com>
2018-09-20 10:08:34 +0200

    Do test to make a zero amount deposit

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class AccountTest {
     public Transactions transaction;
@@ -10,7 +11,9 @@ class AccountTest {
     public void makeDeposit_should_make_a_deposit_of_zero(){
         transaction = mock(Transactions.class);
         Account account = new Account(transaction);
+
         account.makeDeposit(0);
-        assertThat(false).isEqualTo(true);
+
+        verify(transaction).add(0);
     }
 }
