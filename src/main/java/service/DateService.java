e653f65a9f8a19b92ce53d6b07bfb6404ba08ddc
Jacques <jak.rag@gmail.com>
2018-09-20 14:06:27 +0200

    add a class DateService an implementation of Dates who return the date of today

--- src/main/java/service/DateService.java
+++ src/main/java/service/DateService.java
@@ -1,4 +1,12 @@
 package service;
 
-class DateService {
+import utils.Dates;
+
+import java.time.LocalDate;
+
+class DateService implements Dates {
+    @Override
+    public LocalDate getCurrentDate() {
+        return LocalDate.now();
+    }
 }
